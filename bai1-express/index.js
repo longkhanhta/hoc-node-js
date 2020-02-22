62999dd4aa01778f9c8c198415f307ef8fc79847
TaLongKhanh <khanhbaby2k@gmail.com>
2020-02-22 17:52:41 +0700

    bai1 cai sat express

---
+++ bai1-express/index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+
+var app = express();
+
+var port = 3000;
+
+app.get('/', function(req, res) {
+    res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(port, function() {
+    console.log('Server listening on port' + port);
+});
