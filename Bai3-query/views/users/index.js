9a1f14ff23881beabfe545f4c2cafac26a25e48c
TaLongKhanh <khanhbaby2k@gmail.com>
2020-02-23 15:27:52 +0700

    install body-parser

--- Bai3-query/views/users/index.js
+++
@@ -1,7 +0,0 @@
-
-var input = document.getElementById('crrtxt');
-input.value = sessionStorage.getItem('draft');
-input.addEventListener('change', update);
-function update() {
-    sessionStorage.setItem('draft', input.value);
-}
